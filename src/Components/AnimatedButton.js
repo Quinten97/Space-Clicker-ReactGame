2945b36b1477b212004bb6b479e67b631fda91b2
Quinten97 <89324378+Quinten97@users.noreply.github.com>
2021-12-06 00:10:29 -0500

    css update worked on random boost animation and fixed spacing issue with containers

---
+++ src/Components/AnimatedButton.js
@@ -0,0 +1,9 @@
+import "../styles.css";
+
+export default function AnimatedButton() {
+  return (
+    <div>
+      <button className="animated-btn animated-btn-moving">FILLER</button>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import AnimatedButton from "./Components/AnimatedButton";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <div className="app-container">
     <App />
+    <AnimatedButton />
   </div>,
   rootElement
 );
